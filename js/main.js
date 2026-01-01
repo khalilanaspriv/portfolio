7ac9b3435c2860984624a98c552e5e578c20e0fb
Anas <khalil.anas.priv@gmail.com>
2026-01-01 12:09:13 +0100

    feat: enhanced navbar + added dark/light mode toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const navMenuEl = document.querySelector('#nav-menu');
 const navMenuToggleEl = document.querySelector('#nav-menu-toggle');
+const navModeIconEl = document.querySelector('#mode-icon');
+const navModeToggleEl = document.querySelector('#nav-mode-toggle');
 const overlayEl = document.querySelector('#overlay');
+const htmlEl = document.documentElement;
 
 const toggleMenu = () => {
   navMenuEl.classList.toggle('active');
@@ -8,10 +11,25 @@ const toggleMenu = () => {
   const isOpen = navMenuEl.classList.contains('active');
   navMenuToggleEl.setAttribute('aria-expanded', isOpen);
   document.body.style.overflow = isOpen ? 'hidden' : '';
-}
+};
+
+const toggleMode = () => {
+  const currentTheme = htmlEl.getAttribute('data-theme');
+  
+  if (currentTheme === 'dark') {
+    htmlEl.setAttribute('data-theme', 'light');
+    localStorage.setItem('mode', 'light');
+    navModeIconEl.className = 'ri-moon-line';
+  } else {
+    htmlEl.setAttribute('data-theme', 'dark');
+    localStorage.setItem('mode', 'dark');
+    navModeIconEl.className = 'ri-sun-line';
+  }
+};
 
 navMenuToggleEl.addEventListener('click', toggleMenu);
 overlayEl.addEventListener('click', toggleMenu);
+navModeToggleEl.addEventListener('click', toggleMode);
 
 window.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && navMenuEl.classList.contains('active')) {
